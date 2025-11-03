e65eefe395ef5b51c54a3e5a335cf9b92b4bc190
Iskra <77334848+Iskra-YT@users.noreply.github.com>
2025-11-03 20:39:29 +0100

    fix(ci): repair GitHub Actions workflow for CI for Windows

--- build.bat
+++ build.bat
@@ -1,4 +1,7 @@
-rmdir /s /q build
+@echo off
+IF EXIST build (
+    rmdir /s /q build
+)
 mkdir build
 cd build
 cmake ..
--- build.ps1
+++ build.ps1
@@ -1,5 +1,8 @@
-Remove-Item -Recurse -Force build
-New-Item -ItemType Directory -Path build
+if (Test-Path build) {
+    Remove-Item -Recurse -Force build
+}
+
+New-Item -ItemType Directory -Path build -ErrorAction SilentlyContinue | Out-Null
 Set-Location build
 cmake ..
 cmake --build .
