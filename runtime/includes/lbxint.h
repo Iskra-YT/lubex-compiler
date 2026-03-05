33c3147ced3d364353c239a0551ef5039cfe3bea
Iskra-YT <iskra42@proton.me>
2026-03-05 20:53:13 +0100

    feat(runtime): implement binary operator functions for Int class. Fixes #32

--- runtime/includes/lbxint.h
+++ runtime/includes/lbxint.h
@@ -5,4 +5,10 @@ typedef struct {
     double value;
 } _BI_Int;
 
+_BI_Int* _BI_Int_init(double val);
+_BI_Int* _BI_Int_add(_BI_Int* a, _BI_Int* b);
+_BI_Int* _BI_Int_subtract(_BI_Int* a, _BI_Int* b);
+_BI_Int* _BI_Int_multiply(_BI_Int* a, _BI_Int* b);
+_BI_Int* _BI_Int_divide(_BI_Int* a, _BI_Int* b);
+
 #endif // LUBRTX_LIB_LBXINT_H
--- runtime/src/lbxint.c
+++ runtime/src/lbxint.c
@@ -6,3 +6,27 @@ _BI_Int* _BI_Int_init(double val) {
     intVal->value = val;
     return intVal;
 }
+
+_BI_Int* _BI_Int_add(_BI_Int* a, _BI_Int* b) {
+    _BI_Int* c = (_BI_Int*)_BI_malloc(sizeof(_BI_Int));
+    c->value = a->value + b->value;
+    return c;
+}
+
+_BI_Int* _BI_Int_subtract(_BI_Int* a, _BI_Int* b) {
+    _BI_Int* c = (_BI_Int*)_BI_malloc(sizeof(_BI_Int));
+    c->value = a->value - b->value;
+    return c;
+}
+
+_BI_Int* _BI_Int_multiply(_BI_Int* a, _BI_Int* b) {
+    _BI_Int* c = (_BI_Int*)_BI_malloc(sizeof(_BI_Int));
+    c->value = a->value * b->value;
+    return c;
+}
+
+_BI_Int* _BI_Int_divide(_BI_Int* a, _BI_Int* b) {
+    _BI_Int* c = (_BI_Int*)_BI_malloc(sizeof(_BI_Int));
+    c->value = a->value / b->value;
+    return c;
+}
